133a3d3bb970e445f989a4e7c5130f61d6f1c802
Suzzain <gurungsujen@gmail.com>
2020-03-07 13:16:35 +0545

    Initial commit

---
+++ app/src/main/java/com/example/candidate_scenario/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.candidate_scenario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
